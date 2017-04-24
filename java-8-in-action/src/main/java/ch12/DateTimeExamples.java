830616da224aca06d90434d2a52acfd6e94e7363
genius.choi <genius.choi@kakaocorp.com>
2017-04-24 23:54:36 +0900

    DateTimeExamples

---
+++ java-8-in-action/src/main/java/ch12/DateTimeExamples.java
@@ -0,0 +1,10 @@
+package ch12;
+
+import java.time.LocalDate;
+
+public class DateTimeExamples {
+	public static void main(String[] args) {
+		LocalDate date = LocalDate.of(2017, 4, 24);
+		System.out.println(date);
+	}
+}
