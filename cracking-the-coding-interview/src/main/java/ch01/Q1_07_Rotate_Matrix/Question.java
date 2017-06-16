166dfda9b8b1d2efc9a9fb0665a0220c746a6d69
genius.choi <genius.choi@kakaocorp.com>
2017-06-17 01:10:39 +0900

    Q1_07_Rotate_Matrix

--- cracking-the-coding-interview/src/main/java/ch01/Q1_07_Rotate_Matrix/Question.java
+++ cracking-the-coding-interview/src/main/java/ch01/Q1_07_Rotate_Matrix/Question.java
@@ -4,6 +4,11 @@ import CtCILibrary.AssortedMethods;
 
 import java.util.Arrays;
 
+/**
+ * 이미지를 표현하는 N*N 행렬이 있다. 이미지의 각 픽셀은 4바이트로
+ * 표현된다. 이때, 이미지를 90도 회전시키는 메서드를 작성하라. 부가적
+ * 인 행렬을 사용하지 않고서도 할 수 있겠는가?
+ */
 public class Question {
 
 	public static boolean retate(int[][] matrix) {
