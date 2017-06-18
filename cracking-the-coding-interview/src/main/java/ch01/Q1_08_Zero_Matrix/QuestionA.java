6f04f9d9d9155a6524dd5e9e5ebd3b8cd0d698f4
genius.choi <genius.choi@kakaocorp.com>
2017-06-18 11:05:25 +0900

    Q1_08_Zero_Matrix QuestuinA

---
+++ cracking-the-coding-interview/src/main/java/ch01/Q1_08_Zero_Matrix/QuestionA.java
@@ -0,0 +1,33 @@
+package ch01.Q1_08_Zero_Matrix;
+
+/**
+ * M * N 행렬의 한 원소가 0일 경우, 해당 원소가 속한 행과 열의 모든 원
+ * 소를 0으로 설정하는 알고리즘을 작성하라
+ */
+public class QuestionA {
+
+	public static void nullifyRow(int[][] matrix, int row) {
+
+	}
+
+	public static void nullityColumn(int[][] matrix, int col) {
+
+	}
+
+	public static void setZeroz(int[][] matrix) {
+
+	}
+
+	public static boolean matricesAreEqual(int[][] m1, int[][] m2) {
+		return false;
+	}
+
+	public static int[][] cloneMatrix(int[][] matrix) {
+		int[][] c = new int[matrix.length][matrix[0].length];
+		return c;
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
