67bd0a06dc3b968533ca83880f52bef378bea864
genius.choi <genius.choi@meshkorea.net>
2017-10-24 01:51:45 +0900

    Arrays.asList, Set List Remove Overloading

--- java-8-in-action/src/main/java/ch01/TomcatServer.java
+++ java-8-in-action/src/main/java/ch01/TomcatServer.java
@@ -2,13 +2,29 @@ package ch01;
 
 import org.apache.catalina.startup.Bootstrap;
 
-/**
- * Created by Genius on 2017-04-15.
- */
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
 public class TomcatServer {
 
 	public static void main(String[] args) throws Exception {
 		Bootstrap bootstrap = new Bootstrap();
 		bootstrap.start();
+
+		Stream.of("Google", "Naver", "Daum", "google", "Yahoo", "Bing")
+				.filter(s -> s.length() > 4)
+				.map(s -> s.toUpperCase())
+				.distinct()
+				.sorted()
+				.collect(toList());
+
+		int[] ints = new int[]{1, 2, 3};
+		List list1 = Arrays.asList(ints);
+		List list2 = Arrays.asList(1, 2, 3);
+		System.out.println(Arrays.toString((int[]) list1.get(0)));
+		System.out.println(list2);
 	}
 }
