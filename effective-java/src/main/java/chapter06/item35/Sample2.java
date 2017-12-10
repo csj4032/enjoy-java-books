9bbccbb5c07fce4660dcbf1d5964f3baeb34849e
genius.choi <csj4032@gmail.com>
2017-12-10 10:38:11 +0900

    effective java item 35

---
+++ effective-java/src/main/java/chapter06/item35/Sample2.java
@@ -0,0 +1,29 @@
+package chapter06.item35;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sample2 {
+
+	@ExceptionTest(ArithmeticException.class)
+	public static void m1() {
+		int i = 0;
+		i = i / i;
+	}
+
+	@ExceptionTest(ArithmeticException.class)
+	public static void m2() {
+		int[] a = new int[0];
+		int i = a[1];
+	}
+
+	@ExceptionTest(ArithmeticException.class)
+	public static void m3() {
+	}
+
+	@ExceptionTest({IndexOutOfBoundsException.class, NullPointerException.class})
+	public static void doublyBad() {
+		List<String> list = new ArrayList<String>();
+		list.addAll(5, null);
+	}
+}
--- effective-java/src/main/java/chapter06/item35/Test.java
+++ effective-java/src/main/java/chapter06/item35/Test.java
@@ -7,7 +7,5 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-public @interface ExceptionTest {
-
-	Class<? extends Exception>[] value();
+public @interface Test {
 }
