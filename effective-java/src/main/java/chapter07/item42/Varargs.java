ef57510ff010f8899d60c091a7ffe062e5b8139d
genius.choi <csj4032@gmail.com>
2017-12-21 22:25:02 +0900

    effective java item42

--- effective-java/src/main/java/chapter07/item42/Varargs.java
+++ effective-java/src/main/java/chapter07/item42/Varargs.java
@@ -1,5 +1,7 @@
 package chapter07.item42;
 
+import java.util.Arrays;
+
 public class Varargs {
 
 	static int sum(int... args) {
@@ -18,6 +20,11 @@ public class Varargs {
 	}
 
 	public static void main(String[] args) {
+
+		int [][] a = {{1},{2},{3},{4}};
+		System.out.println(Arrays.toString(a));
+		System.out.println(Arrays.deepToString(a));
+
 		System.out.println(sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
 		System.out.println(min(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
 	}
