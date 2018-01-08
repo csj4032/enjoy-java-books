8dd31b13b1654a6b155cbd15ba37be195332d758
genius.choi <genius.choi@wemakeprice.com>
2018-01-08 16:33:09 +0900

    Fibonacci

---
+++ effective-java/src/main/java/chapter10/item66/Fibonacci.java
@@ -0,0 +1,39 @@
+package chapter10.item66;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
+public class Fibonacci extends RecursiveTask<Integer> {
+
+	final int n;
+
+	Fibonacci(int n) {
+		this.n = n;
+	}
+
+	protected Integer compute() {
+		if (n <= 1)
+			return n;
+		Fibonacci f1 = new Fibonacci(n - 1);
+		f1.fork();
+		Fibonacci f2 = new Fibonacci(n - 2);
+		return f2.compute() + f1.join();
+	}
+
+	protected Integer recursive(int n) {
+		if (n <= 1) return n;
+		return recursive(n - 1) + recursive(n - 2);
+	}
+
+	public static void main(String[] args) {
+		Fibonacci fibonacci = new Fibonacci(41);
+		ForkJoinPool pool = new ForkJoinPool();
+		Long start = System.currentTimeMillis();
+		System.out.println(pool.invoke(fibonacci));
+		System.out.println(System.currentTimeMillis() - start);
+
+		start = System.currentTimeMillis();
+		System.out.println(fibonacci.recursive(41));
+		System.out.println(System.currentTimeMillis() - start);
+	}
+}
