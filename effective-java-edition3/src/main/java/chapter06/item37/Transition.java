487f96a86a7c2ef30d323d2622782240d7ff0d72
genius.choi <csj4032@gmail.com>
2019-04-03 22:23:03 +0900

    chapter06

--- effective-java-edition3/src/main/java/chapter06/item37/Transition.java
+++ effective-java-edition3/src/main/java/chapter06/item37/Transition.java
@@ -1,13 +1,13 @@
 package chapter06.item37;
 
+import java.util.ArrayList;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 
 import static chapter06.item37.Phase.*;
-import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.*;
 
 public enum Transition {
 	MELT(SOLID, LIQUID),
@@ -20,13 +20,48 @@ public enum Transition {
 	private final Phase from;
 	private final Phase to;
 
-	private Object obj = Stream.of(values()).collect(groupingBy(t -> t.from, () -> new EnumMap<>(Phase.class), toMap(t -> t.to, t -> t, (x, y) -> y, () -> new EnumMap<>(Phase.class))));
-
 	Transition(Phase from, Phase to) {
 		this.from = from;
 		this.to = to;
 	}
 
+	private static List<Transition> transitions = new ArrayList<>();
+
+	static {
+		Transition melt = Transition.MELT;
+		Transition freeze = Transition.FREEZE;
+		Transition boil = Transition.BOIL;
+		Transition condense = Transition.CONDENSE;
+		Transition sublime = Transition.SUBLIME;
+		Transition deposit = Transition.DEPOSIT;
+		transitions.add(melt);
+		transitions.add(freeze);
+		transitions.add(boil);
+		transitions.add(condense);
+	}
+
+	public static Transition formList(Phase from, Phase to) {
+		Transition transition = null;
+		for (Transition trans : transitions) {
+			if (trans.getFrom().equals(from) && trans.getTo().equals(to)) {
+				transition = trans;
+				break;
+			}
+		}
+		return transition;
+	}
+
+	public static Transition formMap(Phase from, Phase to) {
+		return map.get(from).get(to);
+	}
+
+	private static Map<Phase, Map<Phase, Transition>> map =
+			Stream.of(values()).collect(groupingBy(t -> t.getFrom(), () -> new EnumMap<>(Phase.class),
+					toMap(
+							t -> t.getTo(),
+							t -> t,
+							(x, y) -> x,
+							() -> new EnumMap<>(Phase.class))));
 
 	public Phase getFrom() {
 		return from;
@@ -35,6 +70,4 @@ public enum Transition {
 	public Phase getTo() {
 		return to;
 	}
-
-
 }
--- effective-java-edition3/src/test/java/chapter06/item37/PhaseTest.java
+++ effective-java-edition3/src/test/java/chapter06/item37/PhaseTest.java
@@ -1,8 +1,16 @@
 package chapter06.item37;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import static chapter06.item37.Phase.form;
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static chapter06.item37.Transition.*;
+import static java.util.stream.Collectors.*;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -10,12 +18,27 @@ public class PhaseTest {
 
 	@Test
 	public void from() {
-		//assertThat(form(Phase.SOLID, Phase.LIQUID), is(Transition.MELT));
-		//assertThat(form(Phase.LIQUID, Phase.SOLID), is(Transition.FREEZE));
+		assertThat(formList(Phase.SOLID, Phase.LIQUID), is(Transition.MELT));
+		assertThat(formList(Phase.LIQUID, Phase.SOLID), is(Transition.FREEZE));
+		assertThat(formMap(Phase.LIQUID, Phase.LIQUID), is(Transition.MELT));
+		assertThat(formMap(Phase.LIQUID, Phase.SOLID), is(Transition.FREEZE));
 	}
 
 	@Test
 	public void mapAndList() {
-		System.out.println(Transition.phaseTransitionMap);
+		Transition[] transitions = Transition.values();
+		Map<Phase, List<Transition>> m1 = Stream.of(transitions).collect(groupingBy(t -> t.getFrom()));
+		Map<Phase, List<Transition>> m2 = Stream.of(transitions).collect(groupingBy(t -> t.getFrom(), toList()));
+		Map<Phase, List<Transition>> m3 = Stream.of(transitions).collect(groupingBy(t -> t.getFrom(), HashMap::new, toList()));
+		Map<Phase, List<Transition>> m4 = Stream.of(transitions).collect(groupingBy(t -> t.getFrom(), () -> new EnumMap<>(Phase.class), toList()));
+		Map<Phase, Map<Phase, Transition>> m5 =
+				Stream.of(transitions).collect(groupingBy(t -> t.getFrom(), () -> new EnumMap<>(Phase.class), toMap(t -> t.getTo(), t -> t)));
+		Map<Phase, Map<Phase, Transition>> m6 =
+				Stream.of(transitions).collect(groupingBy(t -> t.getFrom(), () -> new EnumMap<>(Phase.class),
+						toMap(
+								t -> t.getTo(),
+								t -> t,
+								(x, y) -> x,
+								() -> new EnumMap<>(Phase.class))));
 	}
 }
