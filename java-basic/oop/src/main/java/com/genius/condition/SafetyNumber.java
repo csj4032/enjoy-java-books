99e53cdb123b439a0e71f0608967a590e6d03f4c
genius.choi <csj4032@gmail.com>
2019-10-21 15:53:39 +0900

    condition

--- java-basic/oop/src/main/java/com/genius/condition/SafetyNumber.java
+++ java-basic/oop/src/main/java/com/genius/condition/SafetyNumber.java
@@ -21,4 +21,48 @@ public class SafetyNumber {
 				yield new OnRequest(safetyNumberCondition);
 		};
 	}
+
+	private ExpressType getExpressIf(SafetyNumberCondition safetyNumberCondition) {
+		ExpressType expressType = ExpressType.ON_REQUEST;
+		if (safetyNumberCondition.isSafetyNoUseYn()) {
+			if (safetyNumberCondition.getIssueType().equals(IssueType.NEW)) {
+				switch (safetyNumberCondition.getIssueStatus()) {
+					case COMPETE:
+						if (safetyNumberCondition.getExpiredType().equals(ExpiredType.BEFORE)) {
+							expressType = ExpressType.HYPHEN;
+						} else {
+							expressType = ExpressType.COMPLETE_NUMBER;
+						}
+						break;
+					case REQUEST:
+						expressType = ExpressType.ON_REQUEST;
+						break;
+					case FAILURE:
+						expressType = ExpressType.FAILURE;
+						break;
+				}
+			} else if (safetyNumberCondition.getIssueType().equals(IssueType.UPDATE)) {
+				switch (safetyNumberCondition.getIssueStatus()) {
+					case COMPETE:
+						if (safetyNumberCondition.getExpiredType().equals(ExpiredType.BEFORE)) {
+							expressType = ExpressType.COMPLETE_NUMBER;
+						} else {
+							expressType = ExpressType.HYPHEN;
+						}
+						break;
+					case REQUEST:
+						expressType = ExpressType.ON_REQUEST;
+						break;
+					case FAILURE:
+						expressType = ExpressType.FAILURE;
+						break;
+				}
+			} else {
+				expressType = ExpressType.ON_REQUEST;
+			}
+		} else {
+			expressType = ExpressType.NO_REQUEST;
+		}
+		return expressType;
+	}
 }
